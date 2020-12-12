935049a59871caafd659cbf083a2fb329d13a7c8
Ogenna Esimai <ogenna.esimai@mavs.uta.edu>
2020-12-12 11:16:53 -0600

    updated TakePicture btn & look and feel of fragment_profile.xml

--- app/src/main/java/com/example/sharedto_doapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/sharedto_doapp/fragments/ProfileFragment.java
@@ -1,16 +1,24 @@
 package com.example.sharedto_doapp.fragments;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,6 +34,7 @@ import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,6 +53,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -53,8 +63,14 @@ import com.parse.ParseUser;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.bumptech.glide.Glide;
+
+import static android.app.Activity.RESULT_OK;
+import static com.example.sharedto_doapp.fragments.PostFragment.CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE;
+
 public class ProfileFragment extends Fragment {
 
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
     public static final String TAG = "ProfileFragment";
     /*private RecyclerView rvPosts;
     protected PostsAdapter adapter;
@@ -62,6 +78,12 @@ public class ProfileFragment extends Fragment {
     private RecyclerView rvPastTasks;
     protected TaskAdapter adapter;
     protected List<Task> allTasks;
+
+    private Button btnCaptureImage;
+    private File photoFile;
+    private String photoFileName = "photo.jpg";
+    //private Button btnLogOut;
+    private ImageView ivProfileImage;
     public SwipeRefreshLayout swipeContainer;
     private TextView tvNumFriends;
     private TextView tvNumTasksCompleted;
@@ -81,6 +103,14 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         rvPastTasks = view.findViewById(R.id.rvPastTasks);
+        btnCaptureImage = view.findViewById(R.id.btnCaptureImage);
+
+        btnCaptureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
 
         // Steps to use the recycler view:
         // 1. create the adapter
@@ -121,6 +151,64 @@ public class ProfileFragment extends Fragment {
         rvPastTasks.setLayoutManager(new LinearLayoutManager(getContext()));
 
         queryTasks();
+    }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+
+    //this method will be called when the child app returns to the parent app which is the Ogenna_Esimai_Parstagram app
+    //@SuppressLint("RestrictedApi")
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+
+                //ivProfileImage.setImageBitmap(takenImage);
+
+                //Log.
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) { //Uri = Uniform Resource Identifier
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
     }
 
     /*//@Override
